7c6d514314962b2251fab52256ddb7175d7b682a
Tony <laomms@gmail.com>
2019-10-21 12:05:43 +0800

    Add files via upload

---
+++ RemoteHook/Program.cs
@@ -0,0 +1,38 @@
+﻿using EasyHook;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteHook
+{
+    class Program
+    {
+        static String ChannelName = null;
+
+        static void Main(string[] args)
+        {
+            int TargetPID;
+            string targetExe = null;
+            // Will contain the name of the IPC server channel
+            string channelName = null;
+            Process[] localByName = Process.GetProcessesByName("NotePad");
+            TargetPID = localByName[0].Id;
+            RemoteHooking.IpcCreateServer<TestLib.FileMonInterface>(ref ChannelName, WellKnownObjectMode.SingleCall);
+            string injectionLibrary = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "TestLib.dll");
+
+            RemoteHooking.Inject(
+                        TargetPID,
+                        injectionLibrary,
+                        injectionLibrary,
+                        ChannelName);
+            Console.WriteLine("Injected to process {0}", TargetPID);
+            Console.WriteLine("<Press any key to exit>");
+            Console.ReadKey();
+        }
+    }
+}
